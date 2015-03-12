5bf398134b4092cbc2a2add83481c6e8d8fd30f5
Ryan Day <soldair@gmail.com>
2015-03-12 14:30:47 -0700

    example container magic

--- mini-elasticsearch/start.sh
+++ mini-elasticsearch/start.sh
@@ -2,6 +2,13 @@
 
 local ELASTICSEARCH_YML="/opt/elasticsearch/config/elasticsearch.yml"
 
+# load configuration and extra java opts from the volume.
+
+local CUSTOM_YML=/var/lib/elasticsearch/elasticsearch.yml
+if [ -f "$CUSTOM_YML" ]; then
+  ELASTICSEARCH_YML=$CUSTOM_YML
+fi
+
 start() {
 	exec /opt/elasticsearch/bin/elasticsearch -Des.config=$ELASTICSEARCH_YML
 }
